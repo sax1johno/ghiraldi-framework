4957fa497d70b179579cefe02244121cc5d9a0d6
John O'Connor <sax1johno@gmail.com>
2013-01-04 13:10:57 -0500

    Added an install.js file as a central point for installation tasks

---
+++ install.js
@@ -0,0 +1,15 @@
+/**
+ * This is the install file for your new express-powered Ghiraldi app.
+ * If you have tasks that need to be completed prior to running the Ghiraldi framework (such as creating initial
+ * user types or admin users), you can do so using this install script.
+ * 
+ * To use, just place your installation tasks beneath the INSTALL TASKS GO HERE comments and run the install script
+ * when you're ready to install.
+ * 
+ * This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. 
+ * If a copy of the MPL was not distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ * 
+ * Copyright (C) 2012, John O'Connor
+ **/
+ 
+/** INSTALL TASKS GO HERE **/
