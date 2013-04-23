1f597775febc4fe41d841f1154b9f1b3581f17d7
John O'Connor <sax1johno@gmail.com>
2013-04-23 19:03:09 -0400

    Fixed the rosetta index page to correctly get the app views index

--- app/controllers/index.js
+++ app/controllers/index.js
@@ -10,8 +10,10 @@
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 **/
 
+var plugins = require('ghiraldi-plugin-registry').registry;
+
 var index = function(req, res) {
-    res.render('index.jade', {title: 'Ghiraldi'});
+    res.render(plugins.get('app').views.index, {title: 'Ghiraldi'});
 };
 
 module.exports = {
@@ -22,4 +24,4 @@ module.exports = {
             method: index
         }
     ]
-}
+};
